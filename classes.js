eff3f323caec3d23d0064ff761e64322d0ce846d
arte <ardjanve@gmail.com>
2020-06-27 23:15:16 +0200

    initial commit

---
+++ classes.js
@@ -0,0 +1,63 @@
+const speed = 0.1
+
+class Blob {
+	constructor (pos, momentum, radius, attractor) {
+		this.pos = pos
+		this.momentum = momentum
+		this.radius = radius
+		this.attractor = attractor
+	}
+
+
+
+	tryColliding (other) {
+		let distance = this.pos.distanceTo(other.pos)
+		if(distance < this.radius + other.radius){
+
+			this.move(-speed)
+
+			//console.log(this.momentum.sum(other.momentum, -1).dotProduct(this.pos.sum(other.pos, -1)))
+
+			this.momentum.x = (this.momentum.x - 1) * this.momentum.sum(other.momentum, -1).dotProduct(this.pos.sum(other.pos, -1)) * (this.pos.sum(other.pos, -1)) / Math.pow(this.pos.sum(other.pos, -1).length, 2)
+			this.momentum.y = (this.momentum.y - 1) * this.momentum.sum(other.momentum, -1).dotProduct(this.pos.sum(other.pos, -1)) * (this.pos.sum(other.pos, -1)) / Math.pow(this.pos.sum(other.pos, -1).length, 2)
+			other.momentum.x = (other.momentum.x - 1) * other.momentum.sum(this.momentum, -1).dotProduct(other.pos.sum(this.pos, -1)) * (other.pos.sum(this.pos, -1)) / Math.pow(other.pos.sum(this.pos, -1).length, 2)
+			other.momentum.y = (other.momentum.y - 1) * other.momentum.sum(this.momentum, -1).dotProduct(other.pos.sum(this.pos, -1)) * (other.pos.sum(this.pos, -1)) / Math.pow(other.pos.sum(this.pos, -1).length, 2)
+
+		}
+	}
+
+	move (multiplier = speed) {
+		this.pos = this.pos.sum(this.momentum, multiplier)
+	}
+}
+
+class Vector {
+	constructor (x, y) {
+		this.x = x
+		this.y = y
+	}
+
+	length () {
+		return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2))
+	}
+
+	sum (other, multiplier = 1) {
+		//always null?
+		console.log(this.x, other.x, this.y, other.y, multiplier)
+		return new Vector(this.x + other.x * multiplier, this.y + other.y * multiplier)
+	}
+
+	distanceTo (point) {
+		return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2))
+	}
+
+	toAngle () {
+		return Math.tan(this.y / this.x)
+	}
+
+	dotProduct (other) {
+		console.log(this.toAngle(),  other.toAngle())
+		return this.length * other.length * Math.cos(this.toAngle() - other.toAngle())
+	}
+}
+
---
+++ main.js
@@ -0,0 +1,31 @@
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d")
+const width = canvas.width
+const height = canvas.height
+let blobs = []
+
+let blackhole = new Vector(50, 50)
+
+blobs.push(new Blob(new Vector(80, 50), new Vector(-10, 0), 3, blackhole))
+blobs.push(new Blob(new Vector(40, 43), new Vector(10, 0), 5, blackhole))
+
+function tick(){
+
+    ctx.clearRect(0, 0, width, height)
+
+    blobs.forEach(blob => {
+        blob.move(speed)
+        blobs.forEach(other => {
+            if (other != blob){
+                blob.tryColliding(other)
+            }
+        })
+        ctx.beginPath()
+        ctx.arc(width / 100 * blob.pos.x, height / 100 * blob.pos.y, width / 100 * blob.radius, 0, 2 * Math.PI)
+        ctx.fill()
+    })
+
+    window.requestAnimationFrame(tick)
+}
+
+window.requestAnimationFrame(tick)
